261efd7b4d3e44474ed73a3e99f9fee314454542
5may-sytk <189351572+5may-sytk@users.noreply.github.com>
2025-01-16 09:58:23 +0000

    add item.new

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2025_01_16_040406) do
     t.string "name"
     t.text "introduction"
     t.integer "price"
-    t.boolean "is_active"
+    t.boolean "sale_status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
