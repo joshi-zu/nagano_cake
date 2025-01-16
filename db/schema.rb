3d9754037195a7994ceda43e9bd77c7116920d9d
5may-sytk <189351572+5may-sytk@users.noreply.github.com>
2025-01-16 14:47:06 +0000

    item.new 表示変更

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,25 @@ ActiveRecord::Schema.define(version: 2025_01_16_040406) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "customers", force: :cascade do |t|
+    t.string "last_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "postal_code", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "phone_number", default: "", null: false
+    t.string "password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
   create_table "items", force: :cascade do |t|
     t.integer "item_id"
     t.integer "genre_id"
