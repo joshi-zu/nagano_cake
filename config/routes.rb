2135d57929c9c7f121b24e4dc17fde38deea8b97
EC2 Default User <ec2-user@ip-172-31-39-216.ap-northeast-1.compute.internal>
2025-01-18 07:44:31 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions:      'admin/sessions',
     passwords:     'admin/passwords',
-    registrations: 'admin/registrations'
   }
 
   devise_for :customers, skip: [:passwords], controllers: {
