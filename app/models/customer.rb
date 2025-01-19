95df2fc595a08ea220cbf9099631e050c57c6c8b
EC2 Default User <ec2-user@ip-172-31-39-216.ap-northeast-1.compute.internal>
2025-01-19 02:04:15 +0000

    会員情報画面の作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,24 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    last_name + '' + first_name
+  end
+  
+  def full_name_kana
+    last_name_kana + '' + first_name_kana
+  end
+  
+  def customer_status
+    if is_active == true
+      "有効"
+    else
+      "退会"
+    end
+  end
+
+  def active_for_authentication?
+    super && (is_active == true)
+  end
 end
