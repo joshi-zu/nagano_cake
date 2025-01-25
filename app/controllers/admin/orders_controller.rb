b86335841bd3d3322d4e26b840ddb2c3218d02f5
5may-sytk <189351572+5may-sytk@users.noreply.github.com>
2025-01-25 07:34:06 +0000

    履歴一覧修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,16 +8,9 @@ class Admin::OrdersController < ApplicationController
   def show
     @orders = Order.all 
     @order = Order.find(params[:id])
-    @cart_items = CartItem.where(customer_id:(params[:id]))
+    @orders = Order.where(customer_id:(params[:id]))
 
-    ary = []
-    @cart_items.each do |cart_item|
-      ary << cart_item.item.with_tax_price*cart_item.amount
-    end
-    @cart_items_price = ary.sum
     @shipping_cost = 800
-
-    @total_price = @shipping_cost + @cart_items_price
   end
 
   def update
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -21,5 +22,6 @@ module WorkGithub
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
   end
 end
