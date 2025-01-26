31cc81d38cb74eebdbc0985c01ed4a522cdf83aa
5may-sytk <189351572+5may-sytk@users.noreply.github.com>
2025-01-26 04:58:09 +0000

    履歴詳細作成

--- app/controllers/admin/orders_details_controller.rb
+++ app/controllers/admin/orders_details_controller.rb
@@ -1,4 +1,14 @@
 class Admin::OrdersDetailsController < ApplicationController
   def update
+    @order = Order.find(params[:id])
+    @order_detail = @order
+    @order_detail.update(order_detail_params)
+    redirect_to admin_order_path(@order.id)
+  end
+
+  private
+
+  def order_detail_params
+    params.require(:order_detail).permit(:production_status)
   end
 end
